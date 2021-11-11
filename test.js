d7e99fb2fd157450c995e90050cc511bd19c410e
Małgorzata Szymczak <gosia@szymczak.at>
2021-11-11 18:01:30 +0100

    Tried to use date constructor

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
-var myPastDate = new Date(1545, 11, 2, 10, 30, 15);
-var myFutureDate = new Date(2515, 0, 21, 10, 30, 15);
+var birthday = new Date(1994, 4, 15, 8, 40, 57);
+var birthday2 = new Date(1994, 4, 15, 8, 40, 57);
+console.log(birthday.getMonth());
+console.log(birthday.getFullYear());
+console.log(birthday.getDate());
+console.log(birthday.getDay());
+console.log(birthday.getHours());
+console.log(birthday.getTime());
 
-console.log(myPastDate);
-console.log(myFutureDate);
+if(birthday.getTime == birthday2){
+
+    console.log("birthdays are equal")
+}else    
+    console.log("birthdays are not equal")
